d6c48b7e53ed922fcf46357430249c096c7718da
not-notAlex <alex.smith1815@gmail.com>
2020-12-15 18:30:37 +0000

    fixed code

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,8 @@
---Prepares SQL server for dev
+-- sets up sql server for dev
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS hbnb_dev IDENTIFIED BY hbnb_dev_pwd;
+CREATE USER IF NOT EXISTS 'hbnb_dev''@''localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 USE hbnb_dev_db;
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO hbnb_dev;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
 USE performance_schema;
-GRANT SELECT ON performance_schema TO hbnb_dev;
+GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
 FLUSH PRIVILEGES;
