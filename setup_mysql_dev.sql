7646b4943542335ebdfb655f5793114ee6565a00
not-notAlex <alex.smith1815@gmail.com>
2020-12-15 17:58:19 +0000

    added sql setup

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+--Prepares SQL server for dev
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS hbnb_dev IDENTIFIED BY hbnb_dev_pwd;
+USE hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db TO hbnb_dev;
+USE performance_schema;
+GRANT SELECT ON performance_schema TO hbnb_dev;
+FLUSH PRIVILEGES;
