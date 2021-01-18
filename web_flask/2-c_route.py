8359b93a3635836406ac6a4f20f63dae4eafb3eb
1819@holbertonschool.com <1819@holbertonschool.com>
2021-01-18 10:30:29 -0600

    2 blank lines

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,15 +11,18 @@ def hello_hbnb():
     """ Prints "Hello HBNB!" """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ prints "HBNB" """
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """displays "C" followed by the value of the text var"""
     return 'C ' + text.replace('_', ' ')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
