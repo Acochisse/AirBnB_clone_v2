8d13cf6e0a8a39d812f279d2e9f5a8dd5f1d8b26
1819@holbertonschool.com <1819@holbertonschool.com>
2021-01-18 09:54:30 -0600

    adding 0-5 and __init__.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module starts Flask web app
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Prints "Hello HBNB!" """
+    return 'Hello HBNB!'
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """ prints "HBNB" """
+    return 'HBNB'
+
+@app.route('/c/<text>'/, strict_slashes=False)
+def cisfun(text):
+    """displays "C" followed by the value of the text var"""
+    return 'C ' + text.replace('_', ' ')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module starts Flask web app
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Prints "Hello HBNB!" """
+    return 'Hello HBNB!'
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """ prints "HBNB" """
+    return 'HBNB'
+
+@app.route('/c/<text>'/, strict_slashes=False)
+def cisfun(text):
+    """displays "C" followed by the value of the text var"""
+    return 'C ' + text.replace('_', ' ')
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """displays "Python + text var" """
+    return 'Python ' + text.replace('_', ' ')
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """displays N is a number only if is int"""
+    return "{:d} is a number".format(n)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
