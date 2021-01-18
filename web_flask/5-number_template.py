9cfd4b8f50151b815e84d6b91d1774a340d77af5
1819@holbertonschool.com <1819@holbertonschool.com>
2021-01-18 10:40:05 -0600

    added missing '/' to @app.route

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,7 @@ def number(n):
     return "{:d} is a number".format(n)
 
 
-@app.route('number_template/<int n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """displays a HTML page"""
     return render_template('5-number.html', n=n)
