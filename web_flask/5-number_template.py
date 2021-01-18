209565ecf27ab68f9b8a797bcba5f14ddc71c0f5
1819@holbertonschool.com <1819@holbertonschool.com>
2021-01-18 10:35:18 -0600

    pep8

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -11,15 +11,18 @@ def hello_hbnb():
     """ Prints "Hello HBNB!" """
     return 'Hello HBNB!'
 
-@app.route('/', strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ prints "HBNB" """
     return 'HBNB'
 
-@app.route('/c/<text>'/, strict_slashes=False)
+
+@app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """displays "C" followed by the value of the text var"""
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -27,15 +30,18 @@ def pythoniscool(text='is cool'):
     """displays "Python + text var" """
     return 'Python ' + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """displays N is a number only if is int"""
     return "{:d} is a number".format(n)
 
+
 @app.route('number_template/<int n>', strict_slashes=False)
 def number_template(n):
     """displays a HTML page"""
     return render_template('5-number.html', number=n)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
