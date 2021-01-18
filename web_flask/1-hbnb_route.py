275354ea4bb42d18955005e402b2f0fdb4f181f4
1819@holbertonschool.com <1819@holbertonschool.com>
2021-01-18 10:26:24 -0600

    2 blank lines

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ def hello_hbnb():
     """ Prints "Hello HBNB!" """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ prints "HBNB" """
     return 'HBNB'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
