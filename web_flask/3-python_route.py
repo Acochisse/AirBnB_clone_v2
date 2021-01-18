d9bae4ed982489ef592569a01bcd8f892e1728b1
1819@holbertonschool.com <1819@holbertonschool.com>
2021-01-18 10:31:40 -0600

    2 blank lnes && updates from 1 and 2

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -11,21 +11,25 @@ def hello_hbnb():
     """ Prints "Hello HBNB!" """
     return 'Hello HBNB!'
 
-@app.route('/', strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ prints "HBNB" """
     return 'HBNB'
 
-@app.route('/c/<text>'/, strict_slashes=False)
+
+@app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """displays "C" followed by the value of the text var"""
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
     """displays "Python + text var" """
     return 'Python ' + text.replace('_', ' ')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
