08df7867f5d9955dc97e811a71c4563cacb37adc
1819@holbertonschool.com <1819@holbertonschool.com>
2021-01-19 09:56:02 -0600

    adding task 8 files

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module that starts Flask
+"""
+from flask import Flask, render_template
+from models import storage
+from models import *
+app = flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display HTML states page listed A-Z"""
+    states_all = sorted(list(storage.all(State).values()))
+    return (render_templates('7-states_list.html', states_all=states_all))
+
+
+@app.teardown_appcontext
+def teardown():
+    """After request removes current session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
